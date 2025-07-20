7da2001c5edbc76fea926689511a1dc7407a6003
MiggRabbid <mig26@inbox.ru>
2025-07-20 12:53:37 +0300

    Fix: init project

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite';
 import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), tailwindcss()],
+  plugins: [vue(), vueJsx()],
   define: {
     'process.env.MY_ENV_VAR': JSON.stringify(process.env.MY_ENV_VAR),
   },
@@ -31,8 +30,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern-compiler',
-        additionalData: '@use "@/styles/global.styles.scss" as *;\n',
+        additionalData: '@use "@/styles/global.scss" as *;\n',
       },
     },
   },
