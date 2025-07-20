4ba0aefe3458d7f906e7f1aeda776adf8e1aa396
MiggRabbid <mig26@inbox.ru>
2025-07-20 14:24:20 +0300

    Create: add CustomInput

---
+++ src/shared/ui/CustomInput/CustomInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <n-input
+    v-bind="$attrs"
+    :type="type"
+    :placeholder="placeholder"
+    :minlength="maxlength"
+    :maxlength="maxlength"
+    :show-password-on="showPasswordOn"
+    :value="modelValue"
+    @update:value="emit('update:modelValue', $event)"
+  />
+</template>
+
+<script setup lang="ts">
+import { NInput } from 'naive-ui';
+
+defineProps<{
+  modelValue: string;
+  placeholder?: string;
+  type?: 'text' | 'password';
+  minlength?: number;
+  maxlength?: number;
+  showPasswordOn?: 'click' | 'mousedown';
+}>();
+
+const emit = defineEmits<{
+  // eslint-disable-next-line no-unused-vars
+  (e: 'update:modelValue', value: string): void;
+}>();
+</script>
